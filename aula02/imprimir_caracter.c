9675bb70ea77628b672e3c1bc6a6dc2d0937ad64
sophiabpaulino <sophiab.paulino@gmail.com>
2025-03-19 10:51:52 -0300

    Adicionando arquivos da pratica02

---
+++ aula02/imprimir_caracter.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    printf ("%c\n", 'A');
+    printf ("%i\n", 'A');
+    printf ("%s\n", "Cafe");
+
+    return 0;
+}
---
+++ aula02/imprimir_decimais.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    printf ("%.7f\n", 3.421945f);
+    printf ("%4.1f\n", 10.0);
+    printf ("%4.1f\n", 8.9);
+    printf ("R$ %6.2f\n", 219.85);
+    return 0;
+} 
